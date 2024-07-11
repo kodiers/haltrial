7445da846da199b9555cb09a2456a5e7b50347dd
viktoryamchinov <kodiers@gmail.com>
2024-07-12 03:59:12 +0500

    Initial commit

---
+++ src/main/java/com/tfl/haltrial/HaltrialApplication.java
@@ -0,0 +1,13 @@
+package com.tfl.haltrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaltrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaltrialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tfl/haltrial/HaltrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfl.haltrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaltrialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
